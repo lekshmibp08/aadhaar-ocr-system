3468204dae2ded0409ccba22a74672fd9ba948ad
Lekshmi <sreelekshmi310192@gmail.com>
2025-08-25 16:40:12 +0530

    updated header for sending files

--- client/src/service/api.ts
+++ client/src/service/api.ts
@@ -1,6 +1,7 @@
 import configAxios from "./axiosConfig";
 
-
 export const processAndFetchData = (formData: FormData) => {
-    return configAxios.post("/api/ocr/process", formData);
-}
+  return configAxios.post("/api/aadhaar/process", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+};
