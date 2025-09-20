4aa3f924a7b96298519765e60ef977f59722c705
Lekshmi <sreelekshmi310192@gmail.com>
2025-09-20 17:17:59 +0530

    replaced svg with react icon

--- client/src/components/ImageUpload.tsx
+++ client/src/components/ImageUpload.tsx
@@ -1,4 +1,5 @@
 import type React from "react"
+import { HiOutlinePhotograph } from "react-icons/hi";
 
 import { useRef, useState } from "react"
 
@@ -114,21 +115,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
         />
 
         <div className="space-y-2">
-          <svg
-            className="mx-auto h-12 w-12 text-gray-400"
-            stroke="currentColor"
-            fill="none"
-            viewBox="0 0 48 48"
-            aria-hidden="true"
-          >
-            <path
-              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-
+          <HiOutlinePhotograph className="mx-auto h-12 w-12 text-gray-400" />
           <div className="text-gray-600">
             <span className="font-medium text-blue-600 hover:text-blue-500">Click to upload</span>
             <span> or drag and drop</span>
